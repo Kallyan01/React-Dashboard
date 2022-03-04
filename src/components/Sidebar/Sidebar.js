e9ae76ea0800867e165398f82512753a4edfa7a5
Kallyan Singha <singhakallyan@gmail.com>
2022-03-04 23:12:41 +0530

    added sidebar foooter

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,33 +7,54 @@ function Sidebar() {
     const [navOpen, setNavOpen] = nav
     const [Tab, setTab] = tab
 
-        const vis = {
-            opacity: navOpen===true?'1':'0'
-        }
+    const vis = {
+        opacity: navOpen === true ? '1' : '0'
+    }
 
     return (
         <>
-
-            <div className="control">
-                <h3><span>One</span>Ness</h3>
-            </div>
-            <div className="side-search">
-                <i class="fal fa-search"></i>
-                <input type="text" placeholder='Search' />
-
-            </div>
-            <NavLink to='Dashboard' onClick={() => setTab(1)}>
-                <div className="option" style={vis}>
-                    <div className="icon"><i class="fal fa-home"></i></div>
-                    <div className="option-value"><p>Dashboard</p></div>
+            <div className="sidebar-header">
+                <div className="control" style={vis}>
+                    <h3><span>One</span>Ness</h3>
                 </div>
-            </NavLink>
-            <NavLink to='ReleaseProject' onClick={() => setTab(2)}>
-                <div className="option" style={vis}>
-                    <div className="icon"><i class="fal fa-bell"></i></div>
-                    <div className="option-value"><p>Release Project</p></div>
+                <div className="side-search" style={vis}>
+                    <i class="fal fa-search"></i>
+                    <input type="text" placeholder='Search' />
                 </div>
-            </NavLink>
+            </div>
+            <div className="sidebar-body">
+                <NavLink to='Dashboard' onClick={() => setTab(1)}>
+                    <div className="option" style={vis}>
+                        <div className="icon"><i class="fal fa-home"></i></div>
+                        <div className="option-value"><p>Dashboard</p></div>
+                    </div>
+                </NavLink>
+                <NavLink to='ReleaseProject' onClick={() => setTab(2)}>
+                    <div className="option" style={vis}>
+                        <div className="icon"><i class="fal fa-bell"></i></div>
+                        <div className="option-value"><p>Release Project</p></div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className="sidebar-footer">
+                <div className="user-prev">
+                    <div className="user-pro-list">
+                        <div className="userav">
+                            <i class="fal fa-user-circle"></i>
+                        </div>
+                        <div className="userav">
+                            <i class="fal fa-user-circle"></i>
+                        </div>
+                    </div>
+                    <div className="user-invite">
+                        <i class="fal fa-plus"></i>
+                        <h6>Invite</h6>
+                    </div>
+                </div>
+                <div className="app-help">
+                    <i class="fal fa-question"></i>
+                </div>
+            </div>
         </>
     )
 }
